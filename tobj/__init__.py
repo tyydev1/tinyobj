17c776c216614f30abc31e5939dce75b96af22ae
tyydev1 <noobastrox@gmail.com>
2025-12-18 17:07:14 +0700

    .

--- tobj/__init__.py
+++ tobj/__init__.py
@@ -1,5 +1,5 @@
 """
-TinyObj v0.2.1,
+TinyObj v0.2.2,
 a tiny object format for freedom.
 """
 from typing import Any, Dict, Optional, List
@@ -131,5 +131,5 @@ __all__ = [
     "LexerError", "ParserError", "InterpreterError",
 ]
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __author__ = "Razka Rizaldi"
